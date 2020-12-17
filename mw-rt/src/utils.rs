2b05d5b8f6a241b8f26640ebf5f92c914d164026
tiannian <dtiannian@aliyun.com>
2020-12-18 04:46:14 +0800

    fix

--- mw-rt/src/utils.rs
+++ mw-rt/src/utils.rs
@@ -1,6 +1,9 @@
+use alloc::format;
+use mw_std::debug;
+
 #[cfg(target_arch = "wasm32")]
 #[panic_handler]
 fn panic_handler(_panic: &core::panic::PanicInfo) -> ! {
-    // println(format_args!("{:?}", _panic).as_str().unwrap());
+    debug::println(&format!("{}", _panic));
     loop {}
 }
--- mw-std/src/fs.rs
+++ mw-std/src/fs.rs
@@ -1,10 +1,19 @@
+use core::cell::RefCell;
 use core::ffi::c_void;
 use core::future::Future;
+use core::option::Option::Some;
 use core::pin::Pin;
 use core::task::{Context, Poll, Waker};
-use core::option::Option::Some;
-use core::cell::RefCell;
+use alloc::rc::Rc;
+use alloc::format;
 use crate::debug;
+
+unsafe extern "C" fn hook<F>(user_data: *mut c_void, result: u8)
+where
+    F: FnMut(u8),
+{
+    (*(user_data as *mut F))(result)
+}
 
 pub fn read_file_callback<F>(s: &str, mut f: F) -> usize
 where
@@ -13,17 +22,11 @@ where
     #[link(wasm_import_module = "wstd")]
     extern "C" {
         fn _read_file_callback(
-            cb: unsafe extern "C" fn(*mut c_void,u8),
+            cb: unsafe extern "C" fn(*mut c_void, u8),
             user_data: *mut c_void,
             path: *const u8,
             path_len: usize,
         ) -> usize;
-    }
-
-    unsafe extern "C" fn hook<F>(user_data: *mut c_void, result: u8)    where
-        F: FnMut(u8),
-    {
-        (*(user_data as *mut F))(result)
     }
 
     let user_data = &mut f as *mut _ as *mut c_void;
@@ -33,23 +36,24 @@ where
     unsafe { _read_file_callback(hook::<F>, user_data, bytes.as_ptr(), bytes.len()) }
 }
 
-
 #[derive(Debug, Clone)]
 pub struct ReadFile {
-    inner:RefCell<Inner>
+    inner: Rc<RefCell<Inner>>,
 }
 
-#[derive(Debug,Clone)]
-struct Inner{
+#[derive(Debug, Clone, Default)]
+struct Inner {
     result: Option<u8>,
     task: Option<Waker>,
 }
 
-
 impl Default for ReadFile {
     fn default() -> Self {
-        ReadFile{
-            inner: RefCell::new(Inner { result: None, task: None})
+        ReadFile {
+            inner: Rc::new(RefCell::new(Inner {
+                result: None,
+                task: None,
+            })),
         }
     }
 }
@@ -59,15 +63,15 @@ impl Future for ReadFile {
 
     fn poll(self: Pin<&mut Self>, cx: &mut Context) -> Poll<Self::Output> {
         // let mut task = self.task.unwrap();
-        debug::println("poll");
         let mut inner = self.inner.borrow_mut();
+        debug::println("poll");
 
-        if let Some(x) = inner.result{
+        if let Some(x) = inner.result.take() {
             return Poll::Ready(x);
         }
 
         inner.task = Some(cx.waker().clone());
-        debug::println(&alloc::format!("poll inner ptr:{:?}",self.inner.as_ptr()));
+
         Poll::Pending
     }
 }
@@ -78,25 +82,25 @@ pub fn read_file(s: &str) -> ReadFile {
     //由于第一次poll执行失败，waker没传出来，导致回调了也无法唤醒第二次poll
     let fu = ReadFile::default();
     let inner = fu.inner.as_ptr();
-    unsafe {
-        read_file_callback(s,   move|result:u8| {
+    read_file_callback(s, move |result: u8| {
+        debug::println(&format!("{:?}", inner));
+        unsafe {
             let inner = &mut *inner;
             inner.result = Some(result);
             let task_op = &inner.task.as_ref();
             if task_op.is_some() {
                 task_op.unwrap().wake_by_ref()
             };
-        });
-    }
-
-    ReadFile::default()
+        }
+    });
+    fu
 }
 
 #[no_mangle]
 pub extern "C" fn call_read_file_callback_fn(
-    cb: unsafe extern "C" fn(*mut c_void,u8),
+    cb: unsafe extern "C" fn(*mut c_void, u8),
     user_data: *mut c_void,
-    result:u8
+    result: u8,
 ) {
-    unsafe { cb(user_data,result) }
+    unsafe { cb(user_data, result) }
 }
